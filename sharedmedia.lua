c9f80595811d62db522d404c790cf05b9e158161
Andre <asalmeri@stud.hs-bremen.de>
2018-08-18 23:12:05 +0200

    Update v1.01
    ** Updated layout
    ** QuestLog started
    ** General Font update
    ** Misc Code changes
    ** Cleaned up install process

--- sharedmedia.lua
+++ sharedmedia.lua
@@ -1,7 +1,7 @@
 ﻿local LSM = LibStub("LibSharedMedia-3.0")
 
 -- fonts
-LSM:Register("font", "Crescent-Regular", [[Interface\Addons\ElvUI_HTUI\assets\ElvUI B.ttf]])
+LSM:Register("font", "Big Noodle Tilting", [[Interface\Addons\ElvUI_HTUI\assets\ElvUI B.ttf]])
 
 --statusbars
 LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\ElvUI_HTUI\assets\ElvUI I.tga]])
